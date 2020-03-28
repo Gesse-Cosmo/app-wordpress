291d2f74e8cc375db7b3b124c7e81626c5cbc755
Gesse-Cosmo <48337331+Gesse-Cosmo@users.noreply.github.com>
2020-03-28 12:50:57 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : '39',
+		wp_ws_url : 'http://localhost/escrevendo/wp-appkit-api/39',
+		wp_url : 'http://localhost/escrevendo',
+		theme : 'q-android',
+		version : '0.1',
+		app_type : 'phonegap-build',
+		app_title : 'Escrevendo',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : -3,
+		debug_mode : 'off',
+		auth_key : 'T-6Jd^j^u|]Tzff~!7`J>aN></*iX:1G_6iP!n^6),m(WXF;c#1(Uk`rvHurU;en',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
